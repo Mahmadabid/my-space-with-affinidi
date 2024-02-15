de9162b8e04602f5f61cb575d5a7a2fa455b1651
Muhammad Ahmad <75790323+Mahmadabid@users.noreply.github.com>
2024-02-15 21:58:24 +0500

    Update bank.ts

--- src/pages/api/bank.ts
+++ src/pages/api/bank.ts
@@ -6,6 +6,11 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
+    if (req.method === 'OPTIONS') {
+        res.status(200).end();
+        return;
+    }
+
     res.setHeader('Access-Control-Allow-Origin', 'https://pixels-market.vercel.app');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
