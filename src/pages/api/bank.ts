0f838930e0d3b0b74b7c53d6d66423d66dfb058a
Muhammad Ahmad <75790323+Mahmadabid@users.noreply.github.com>
2024-02-15 21:24:31 +0500

    Update bank.ts

--- src/pages/api/bank.ts
+++ src/pages/api/bank.ts
@@ -6,7 +6,7 @@ export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
 
