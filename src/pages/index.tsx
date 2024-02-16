e3978ba31e25949a2f25e46fd221df1847c89955
Muhammad Ahmad <75790323+Mahmadabid@users.noreply.github.com>
2024-02-16 08:53:18 +0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,7 +14,7 @@ const Home = () => {
       <div className="mx-auto bg-sky-50 my-3 px-4 py-6 rounded-lg shadow-md max-w-lg">
         <h1 className="font-bold text-3xl text-blue-500 italic text-center mb-4">Welcome, {User.user.nickname ? User.user.nickname : User.user.givenName ? User.user.givenName : 'User'}</h1>
         <p className="mb-4">My Space offers you access to a variety of features all in one place. Whether you need to track expenses, manage tasks, or organize bookmarks, we've got you covered.</p>
-        <p className="mb-4">But wait, there's more! My Space also includes a banking feature. You can send money to anyone and use your bank account to purchase items in <Link href="https://pixels-market.vercel.app/" className="text-blue-500 font-medium">Pixel Market</Link>.</p>
+        <p className="mb-4">But wait, there's more! My Space also includes a banking feature. You can send money to anyone and use your bank account to purchase items in <Link href="https://pixels-market.vercel.app/" target="_blank" className="text-blue-500 font-medium">Pixel Market</Link>.</p>
       </div>
       <div className="flex flex-wrap justify-center space-x-7 items-center">
         <div className="bg-teal-100 my-3 px-4 py-6 rounded-lg shadow-md min-w-64 max-w-96">
@@ -22,7 +22,7 @@ const Home = () => {
             <FontAwesomeIcon icon={faMoneyBillTransfer} className="w-10 h-10" />
             <h1 className="font-bold text-3xl text-blue-500 italic text-center">Bank</h1>
           </div>
-          <p className="mb-4">The Pixel Bank allows you to send money to your friends, family and other people. It also allows you to buy stuff directly from <Link href="https://pixels-market.vercel.app/" className="text-blue-500 font-medium">Pixel Market</Link>. You also get a signup bonus.</p>
+          <p className="mb-4">The Pixel Bank allows you to send money to your friends, family and other people. It also allows you to buy stuff directly from <Link href="https://pixels-market.vercel.app/" target="_blank" className="text-blue-500 font-medium">Pixel Market</Link>. You also get a signup bonus.</p>
           <div className="flex justify-center items-center">
             <Link href="/bank">
               <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
@@ -38,9 +38,9 @@ const Home = () => {
           </div>
           <p className="mb-4">Create your business card directly from card maker. Data will be taken from your affinidi vault and added auomatically to save you time.</p>
           <div className="flex justify-center items-center">
-            <Link href="/bank">
+            <Link href="/card">
               <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                Go to Bank
+                Go to Card Maker
               </button>
             </Link>
           </div>
@@ -52,9 +52,9 @@ const Home = () => {
           </div>
           <p className="mb-4">Stay organized and on top of your tasks with our intuitive Todo feature. Manage your to-do list effortlessly. Open them from other devices. Your data is safe with us</p>
           <div className="flex justify-center items-center">
-            <Link href="/bank">
+            <Link href="/todo">
               <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                Go to Bank
+                Go to Todo
               </button>
             </Link>
           </div>
@@ -66,9 +66,9 @@ const Home = () => {
           </div>
           <p className="mb-4">Track your expenses and stay within budget using our handy Expense Tracker. Manage your finances with ease. Open them from other devices. Your data is safe with us</p>
           <div className="flex justify-center items-center">
-            <Link href="/bank">
+            <Link href="/expense">
               <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                Go to Bank
+                Go to Expense Tracker
               </button>
             </Link>
           </div>
@@ -80,9 +80,9 @@ const Home = () => {
           </div>
           <p className="mb-4">Organize your favorite websites and resources with our Bookmark feature. Never lose track of important links again. Open them from other devices. Your data is safe with us.</p>
           <div className="flex justify-center items-center">
-            <Link href="/bank">
+            <Link href="/bookmark">
               <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-                Go to Bank
+                Go to Bookmarks
               </button>
             </Link>
           </div>
